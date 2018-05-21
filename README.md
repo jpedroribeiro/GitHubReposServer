bceed9035494828cf8bab35f57bd2b80d114c8d0
J. Pedro Ribeiro <joaopar@gmail.com>
2018-05-21 11:00:42 +0100

    Getting node server to output json

---
+++ README.md
@@ -0,0 +1,12 @@
+# GitHub Repositories Server
+
+This is a very simple ExpressJS server that connects to GitHub's GraphQL API and outputs its data.
+
+This approach was mostly used to obfuscate auth keys for the client side app (currently under construction).
+
+### Note to self: when using Heroku...
+
+    git add .
+    git commit -m "..."
+    git push origin master
+    git push heroku master
--- index.js
+++ index.js
@@ -1,7 +1,50 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const query = `
+	query {
+		user(login: "jpedroribeiro") {
+			name
+			url
+			bio
+			avatarUrl
+			location
+			repositories (isLocked: false, privacy: PUBLIC, first: 100, orderBy: {field: CREATED_AT, direction: DESC} ){
+				nodes (){
+					id
+					name
+					createdAt
+					description
+					url
+					primaryLanguage (){
+						name
+					}
+					pushedAt
+					repositoryTopics (first: 100){
+						nodes{
+							topic {
+								name
+							}
+						}
+					}
+				}
+			}
+		}
+	}`;
 
-express()
-	.get('/', (req, res) => res.json({ foo: 'bar' }))
-	.listen(PORT, () => console.log(`Listening on ${PORT}`));
+fetch('https://api.github.com/graphql', {
+	method: 'POST',
+	body: JSON.stringify({ query }),
+	headers: {
+		Authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`
+	}
+})
+	.then(res => res.json())
+	.then(body => {
+		express()
+			.get('/', (req, res) => res.json(body))
+			.listen(PORT, () => console.log(`Listening on ${PORT}`));
+	})
+	.catch(error => console.error(error));
