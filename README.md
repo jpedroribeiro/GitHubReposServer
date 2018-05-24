a4754e5861ebcf103b13080c92c7a8272b853638
J. Pedro Ribeiro <joaopar@gmail.com>
2018-05-24 14:29:16 +0100

    Setting up my own GraphQL server

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # GitHub Repositories Server
 
-This is a very simple ExpressJS server that connects to GitHub's GraphQL API and outputs its data.
+This is a very simple ExpressJS server that connects to GitHub's GraphQL API and outputs its data via its own GraphQL server.
 
-This approach was mostly used to obfuscate auth keys for the [client side app](https://github.com/jpedroribeiro/GitHubReposClient).
+This approach was mostly used to obfuscate auth keys for the [client side app](https://github.com/jpedroribeiro/GitHubReposClient). It's not an ideal solution for production but good enough for playing around with the technologies.
 
 # Usage
 
@@ -13,6 +13,12 @@ Create a `.env` file with the following:
     ORIGIN=YOUR_CLIENT_ORIGIN
 
 Replacing `YOUR_TOKEN` with a token you got via your [GitHub Developer Settings](https://github.com/settings/tokens), `USER` with any GitHub username, like mine `jpedroribeiro`, and `ORIGIN` for your cient's origin, for example `http://localhost:3000`.
+
+Then:
+
+    $ node index.js
+
+Go to `http://localhost:3000/graphiql` for GraphiQL explorer, or `http://localhost:3000/graphql` for its endpoint.
 
 ### Note to self: when using Heroku...
 
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
+// Load up environment variables (check README.md for more details)
 require('dotenv').config();
-const fetch = require('node-fetch');
-const cors = require('cors');
-const corsOptions = {
-	origin: process.env.ORIGIN,
-	optionsSuccessStatus: 200
-};
 
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+// Import fetch polyfil for node
+const fetch = require('node-fetch');
+
+// Our GraphQL query for GitHub's API
 const query = `
 	query {
 		user(login: "${process.env.GITHUB_PROFILE}") {
@@ -40,6 +36,7 @@ const query = `
 		}
 	}`;
 
+// Fetching data through GitHub's API
 fetch('https://api.github.com/graphql', {
 	method: 'POST',
 	body: JSON.stringify({ query }),
@@ -49,8 +46,124 @@ fetch('https://api.github.com/graphql', {
 })
 	.then(res => res.json())
 	.then(body => {
+		// Import dependencies
+		const express = require('express');
+		const bodyParser = require('body-parser');
+		const { makeExecutableSchema } = require('graphql-tools');
+		const {
+			graphqlExpress,
+			graphiqlExpress
+		} = require('apollo-server-express');
+
+		// Import and set CORS config for our own server
+		const cors = require('cors');
+		const corsOptions = {
+			origin: process.env.ORIGIN,
+			optionsSuccessStatus: 200
+		};
+
+		// Which port we're listening to
+		const PORT = process.env.PORT || 5000;
+
+		// Copying data from GitHub's API to variables
+		// 1/2) Author data
+		const author = {
+			name: body.data.user.name,
+			url: body.data.user.url,
+			bio: body.data.user.bio,
+			avatarUrl: body.data.user.avatarUrl,
+			location: body.data.user.location
+		};
+
+		// 2/2) Repositories data
+		const repositories = [];
+		body.data.user.repositories.nodes.forEach(element => {
+			function reduceTopicsObjectsToAnArray(nodes) {
+				let topicsArray = [];
+				nodes.forEach(node => {
+					topicsArray.push(node.topic.name);
+				});
+
+				return topicsArray;
+			}
+			const repo = {
+				...element,
+				primaryLanguage: element.primaryLanguage
+					? element.primaryLanguage.name
+					: '',
+				repositoryTopics: reduceTopicsObjectsToAnArray(
+					element.repositoryTopics.nodes
+				)
+			};
+			repositories.push(repo);
+		});
+
+		// Our own GraphQL type definition
+		const typeDefs = `
+    type Author {
+      name: String
+      url: String
+      bio: String
+      avatarUrl: String
+      location: String
+    }
+
+    type Repository {
+      id: String
+      name: String
+      createdAt: String
+      description: String
+      url: String
+      pushedAt: String
+      primaryLanguage: String
+      topics: [Topic]
+    }
+
+    type Topic {
+      name: String
+    }
+
+    type Query {
+      author: Author
+      repository(id: String): Repository
+      repositories: [Repository]
+    }
+  `;
+
+		// Our own GraphQL resolvers
+		const resolvers = {
+			Query: {
+				author: () => author,
+				repository: (root, search) =>
+					repositories.filter(repo => repo.id == search.id)[0],
+				repositories: () => repositories
+			},
+			Repository: {
+				topics: repository => {
+					let topics = [];
+					repository.repositoryTopics.forEach(topic => {
+						topics.push(topic);
+					});
+					return topics;
+				}
+			},
+			Topic: {
+				name: name => name
+			}
+		};
+
+		// Mix it up into a GraphQL schema
+		const schema = makeExecutableSchema({
+			typeDefs,
+			resolvers
+		});
+
+		// Build up the server for our GraphQL endpoint
 		express()
-			.get('/', cors(corsOptions), (req, res) => res.json(body))
-			.listen(PORT, () => console.log(`Listening on ${PORT}`));
+			// The GraphQL endpoint
+			.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+			// GraphiQL, a visual editor for queries
+			.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+			.listen(PORT, cors(corsOptions), () => console.log('server running'));
 	})
 	.catch(error => console.error(error));
